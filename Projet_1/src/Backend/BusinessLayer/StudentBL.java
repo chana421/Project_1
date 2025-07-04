1603a7fedef3cfa5cc68eccf49031c0750ac21c7
Chana <chana421@gmail.com>
2025-07-04 18:49:31 +0300

    update student

---
+++ Projet_1/src/Backend/BusinessLayer/StudentBL.java
@@ -0,0 +1,12 @@
+package Backend.BusinessLayer;
+
+public class StudentBL {
+    private String name;
+    private int id;
+
+
+
+    public StudentBL(String name, int id){
+        this
+    }
+}
--- Projet_1/src/Backend/BusinessLayer/StudentFacade.java
+++ Projet_1/src/Backend/BusinessLayer/StudentFacade.java
@@ -1,3 +1,5 @@
+package Backend.BusinessLayer;
+
 class StudentFacade{
     
     
