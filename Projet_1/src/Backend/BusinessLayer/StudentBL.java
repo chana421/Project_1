767a546b3a57469201c344f9b97cbf6da92d18b5
Chana <chana421@gmail.com>
2025-07-07 10:49:02 +0300

    add initial project documents for Cyber Mamriot

--- Projet_1/src/Backend/BusinessLayer/StudentBL.java
+++ Projet_1/src/Backend/BusinessLayer/StudentBL.java
@@ -1,12 +1,83 @@
 package Backend.BusinessLayer;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.jar.Attributes.Name;
 
 public class StudentBL {
     private String name;
     private int id;
+    private String location;
+    private int NumLessons;
+    private int NumTest;
+    private LocalDate startlearn;
+    private LocalDate staplearn;
+    private String status;
+    private double balance;
+
+    private List<String> lessonLog;  
+    private List<String> achievements; 
+
+    private 
+
+
+
+
 
 
 
     public StudentBL(String name, int id){
-        this
+        this.name=name;
+        this.id=id;
+        this.NumLessons=0;
+        this.NumTest=0;
+
     }
+     public StudentBL(String name, int id,int NumLessons,int NumTest){
+        this.name=name;
+        this.id=id;
+        this.NumLessons=NumLessons;
+        this.NumTest=NumTest;
+
+    }
+       public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getNumLessons() {
+        return NumLessons;
+    }
+
+    public int getNumTest() {
+        return NumTest;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setNumLessons(int numLessons) {
+        this.NumLessons = numLessons;
+    }
+
+    public void setNumTest(int numTest) {
+        this.NumTest = numTest;
+    }
+
 }
--- Projet_1/src/Backend/BusinessLayer/StudentFacade.java
+++ Projet_1/src/Backend/BusinessLayer/StudentFacade.java
@@ -1,7 +1,22 @@
 package Backend.BusinessLayer;
 
-class StudentFacade{
-    
-    
+import java.io.EOFException;
+import java.util.HashMap;
+import java.util.Map;
 
+class StudentFacade{
+    private Map<Integer, StudentBL> studentsMap;
+
+    public StudentFacade(){
+        this.studentsMap=new HashMap<>();
+    }
+    
+    public StudentBL getStudent(int id){
+        if(!studentsMap.containsKey(id))
+          throw new IllegalArgumentException("student is not exsit");
+       
+          return studentsMap.get(id);
+    }
+
+    
 }
