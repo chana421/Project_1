d57c42b35a9008edcd0a08456b89d6b14eede114
nayotc <nayot@post.bgu.ac.il>
2025-07-07 11:41:30 +0300

    refactor: remove unused id field and commented-out calenderMap in CalenderBL class

--- Projet_1/src/Backend/BusinessLayer/CalenderBL.java
+++ Projet_1/src/Backend/BusinessLayer/CalenderBL.java
@@ -3,15 +3,14 @@ import java.time.LocalDate;
 import java.util.List;
 import java.time.LocalTime;
 import java.util.Map;
+import java.util.AbstractMap;
 import java.util.HashMap;
 
 public class CalenderBL {
 
-    private int id; 
-    private Map< LocalTime, String > calenderMap;
-
+    //private Map< LocalTime, String > calenderMap;
+  private Map <LocalDate, AbstractMap.SimpleEntry<String, Integer>> calenderMap;
     public CalenderBL() {
-        this.id = id;
         this.calenderMap = new HashMap<>();
     }    
 
